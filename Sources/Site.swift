543f9f8bcd6eb1c0f63ee3ff4783913a46d94726
Juris Andersons <>
2025-01-30 23:10:27 +0200

    github url

--- Sources/Site.swift
+++ Sources/Site.swift
@@ -2,10 +2,9 @@ import Foundation
 import Ignite
 
 @main
-struct IgniteWebsite {
+struct Website {
     static func main() async {
-        let site = ExampleSite()
-
+        let site = AkordeonsSite()
         do {
             try await site.publish()
         } catch {
@@ -14,16 +13,14 @@ struct IgniteWebsite {
     }
 }
 
-struct ExampleSite: Site {    
+struct AkordeonsSite: Site {
     var name = "akordeons.lv"
     var titleSuffix = " – akordeons.lv"
-    var url = URL(static: "https://coreteameu.github.io")
-    var builtInIconsEnabled = true
+    var url = URL(static: "https://coreteameu.github.io/akordeons/") // github url
+    var builtInIconsEnabled = false
 
     var author = "Juris Andersons"
 
     var homePage = Home()
     var layout = MainLayout()
 }
-
-
